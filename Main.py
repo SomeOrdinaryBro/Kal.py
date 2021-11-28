8dba98d127cf1cf299af4008513d78f86c303505
Sajid S <95146062+SajidS9@users.noreply.github.com>
2021-11-28 12:54:25 +0530

    Main.py
    Output text has color now

--- Main.py
+++ Main.py
@@ -1,3 +1,16 @@
+class color:
+   PURPLE = '\033[95m'
+   CYAN = '\033[96m'
+   DARKCYAN = '\033[36m'
+   BLUE = '\033[94m'
+   GREEN = '\033[92m'
+   YELLOW = '\033[93m'
+   RED = '\033[91m'
+   BOLD = '\033[1m'
+   UNDERLINE = '\033[4m'
+   END = '\033[0m'
+
+
 #calculating the salary perday
 PRHR = input('How Much Do You Get Paid Per-Hour: ')
 HRSPD = input('How Many Hours do you Work Per-day: ')
@@ -6,8 +19,7 @@ PRDAYSAL = float(PRHR) * float(HRSPD)
 #calculating the salary permonth using 'PRDAYSAL' from above code
 DYSLBR = input('How manys did you work this month? ')
 
-#Using DYSLBR multiplied to PRDAYSAL the code below will find the users monthly salary & per day salary
-print(PRDAYSAL, 'Is your Per Day Salary')
-
 MTHLYPAY = float(PRDAYSAL) * float(DYSLBR)
-print(MTHLYPAY, 'this Should be your Salary This Month')
+#print(MTHLYPAY, 'this Should be your Salary This Month')
+
+print(MTHLYPAY, (color.GREEN + 'is your Monthly Salary' + color.END))
