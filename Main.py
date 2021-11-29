02846c2e34d8c2aa3e88a6711836722fb6a775ef
Axiata <95146062+SomeOrdinaryBro@users.noreply.github.com>
2021-11-29 13:41:44 +0530

    Update Main.py
    added EPF cuts

--- Main.py
+++ Main.py
@@ -18,6 +18,17 @@ DYSLBR = input('How manys did you work this month? ')
 
 MTHLYPAY = int(PRDAYSAL) * int(DYSLBR)
 
+
+EPF = input('Does your workplace require you to contribute to EPF? (please type in uppercase YES/NO) ')
+
+if EPF == "YES": 
+   EPFCUTS = 8
+   MTHLYPAYSS = MTHLYPAY
+   SALWITHEPF = int(MTHLYPAYSS) - int(MTHLYPAYSS) * float(EPFCUTS) / 100
+
+
+
+
 #NOT USING THIS SINCE IM USING A ASCII OUTPUT BELOW... SORT OF..
 #print(PRDAYSAL, (color.GREEN + 'is your per-day salary' + color.END))
 #print(MTHLYPAY, (color.GREEN + 'is your Monthly Salary' + color.END))
@@ -26,5 +37,6 @@ print("\n\n")
 print("     Y O U R - S A L A R Y - D E T A I L S ")
 print("==============================================")
 print("YOUR PER-DAY SALARY: ", float(PRDAYSAL), (color.GREEN + 'is your per-day salary' + color.END))
-print("YOUR MONTHLY SALARY: ", int(MTHLYPAY), (color.GREEN + 'is your Monthly Salary' + color.END))
+print("YOUR BASIC SALARY: ", int(MTHLYPAY), (color.GREEN + 'is your Monthly Salary' + color.END))
+print("YOUR MONTHLY SALARY: ", float(SALWITHEPF), (color.GREEN + 'is your Salary in hand with Contribution to EPF' + color.END))
 print("==============================================")
