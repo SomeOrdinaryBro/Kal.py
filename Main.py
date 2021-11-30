032dee026ad08d35dd14f1e1433935352ce8b843
Axiata <95146062+SomeOrdinaryBro@users.noreply.github.com>
2021-11-30 22:15:35 +0530

    Update Main.py
    Fixed the lowercase lettors not working

--- Main.py
+++ Main.py
@@ -1,4 +1,4 @@
-def SALARYDETAILSTOPRINTWITHEPF():
+def CAPITAL_OUTPUT1():
    print("\n\n")
    print("          Y O U R      -      S A L A R Y -      D E T A I L S ")
    print("================================================================================")
@@ -7,7 +7,7 @@ def SALARYDETAILSTOPRINTWITHEPF():
    print("YOUR MONTHLY SALARY: ", float(SALWITHEPF), (color.GREEN + 'is your Salary in hand with 8% Contribution to EPF' + color.END))
    print("================================================================================")
 
-def SALARYDETAILSWITHOUTEPF():
+def CAPITAL_OUTPUT2():
    print("\n\n")
    print("          Y O U R      -      S A L A R Y -      D E T A I L S ")
    print("================================================================================")
@@ -16,16 +16,17 @@ def SALARYDETAILSWITHOUTEPF():
    print("================================================================================")
 
 
+
+
+
+
+#NOT USING THIS SINCE IM USING A ASCII OUTPUT BELOW... SORT OF..
+#print(PRDAYSAL, (color.GREEN + 'is your per-day salary' + color.END))
+#print(MTHLYPAY, (color.GREEN + 'is your Monthly Salary' + color.END))
+
 class color:
-   PURPLE = '\033[95m'
-   CYAN = '\033[96m'
-   DARKCYAN = '\033[36m'
-   BLUE = '\033[94m'
    GREEN = '\033[92m'
-   YELLOW = '\033[93m'
-   RED = '\033[91m'
    BOLD = '\033[1m'
-   UNDERLINE = '\033[4m'
    END = '\033[0m'
 
 PRHR = input('How Much Do You Get Paid Per-Hour: ')
@@ -37,19 +38,27 @@ DYSLBR = input('How manys did you work this month? ')
 MTHLYPAY = int(PRDAYSAL) * int(DYSLBR)
 
 
-EPF = input('Does your workplace require you to contribute to EPF? (please type in uppercase YES/NO) ')
+EPF = input('Does your workplace require you to contribute to EPF? (please type YES/NO) ')
 
 if EPF == "YES": 
    EPFCUTS = 8
    MTHLYPAYSS = MTHLYPAY
    SALWITHEPF = int(MTHLYPAYSS) - int(MTHLYPAYSS) * float(EPFCUTS) / 100
-#NOT USING THIS SINCE IM USING A ASCII OUTPUT BELOW... SORT OF..
-#print(PRDAYSAL, (color.GREEN + 'is your per-day salary' + color.END))
-#print(MTHLYPAY, (color.GREEN + 'is your Monthly Salary' + color.END))
 
-   SALARYDETAILSTOPRINTWITHEPF()
+   CAPITAL_OUTPUT1()
 
 elif EPF == ("NO"):
-   SALARYDETAILSWITHOUTEPF()
-   
-   
+   CAPITAL_OUTPUT2()
+
+
+if EPF == "yes": 
+   EPFCUTS = 8
+   MTHLYPAYSS = MTHLYPAY
+   SALWITHEPF = int(MTHLYPAYSS) - int(MTHLYPAYSS) * float(EPFCUTS) / 100
+
+   CAPITAL_OUTPUT1()
+
+elif EPF == ("no"):
+   CAPITAL_OUTPUT2()
+
+input()
